8c452113abe83e4f11c4f6c719bf19264497588f
Андрей Прокофьев <65781959+kensoi@users.noreply.github.com>
2023-07-18 15:18:45 +0500

    Repl.It костыль

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ import asyncio
 from dotenv import load_dotenv
 from bot import bot
 
+from background import keep_alive
+
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
 
 if __name__ == "__main__":
     load_dotenv()
+    keep_alive()
     loop.run_until_complete(bot())
