11b28466fbe11251874d9d0fd2b18368d24abb4f
Андрей Прокофьев <65781959+kensoi@users.noreply.github.com>
2023-07-18 13:45:35 +0500

    Update get_started_lib.py

--- library/get_started_lib.py
+++ library/get_started_lib.py
@@ -5,13 +5,38 @@ Copyright 2022 kensoi
 from vkbotkit.objects import callback, Library
 from vkbotkit.objects.filters.actions import ChatInviteUser
 from vkbotkit.objects.filters.message import IsThatText, IsCommand
-HELLO_MESSAGE = """
-Hello, world
-Programmed to work and not to feel
-Not even sure that this is real
+
+class Hello:
+    condition = IsCommand({"старт",}) | IsThatText({"Начать", "начать"}) | ChatInviteUser()
+    message = """
+Привет, пользователь!
+
+Мы рады вашему вступлению! На сей раз правил нет, но в будущем они могут появиться! Следите за тегом #правила в этой беседе, чтобы узнать больше.
 """
-HELP_MESSAGE = """
-HELP INSTRUCTIONS HERE
+
+class Help:
+    condition = IsCommand({"помощь",}) | IsThatText({"Помощь", "помощь"})
+    message = """
+#помощь
+
+Правил нет, наслаждайтесь свободой. Пока что.
+"""
+
+class Admin:
+    condition = IsCommand({"админы",}) | IsThatText({"Кто админ", "кто админ", "Кто админ?", "кто админ?"})
+    message = """
+Пользователь, всем в беседе заведует [id517114114|Андрей Прокофьев]
+"""
+
+class End:
+    condition = IsCommand({"выход",})
+    
+    class messages:
+        ok = """
+Хорошо, завершаю работу.
+"""
+        no_rights = """
+У вас нет прав в системе чатбота, чтобы провернуть эту аферу.
 """
 
 
@@ -20,19 +45,30 @@ class Main(Library):
     Библиотека с командами, предназначенными для приветствия пользователей
     """
 
-    @callback(IsCommand({"start",}) | IsThatText({"Начать",}) | ChatInviteUser())
+    @callback(Hello.condition)
     async def send_hello(self, toolkit, package):
-        """
-        при получении команды '@your_bot_id start' => отправлять текст HELLO_ME
-        """
-        await toolkit.messages.send(package, HELLO_MESSAGE)
+        await toolkit.messages.send(package, Hello.message)
 
 
-    @callback(IsCommand({"help",}) | IsThatText({"Помощь",}))
+    @callback(Help.condition)
     async def send_help(self, toolkit, package):
-        """
-        при получении команды '@your_bot_id start' => отправлять текст HELLO_ME
-        """
+        await toolkit.messages.send(package, Help.message)
 
-        await toolkit.messages.send(package, HELP_MESSAGE)
+        
+    @callback(Admin.condition)
+    async def send_admin_list(self, toolkit, package):
+        await toolkit.messages.send(package, Admin.message)
+
+        
+    @callback(End.condition)
+    async def end_bot(self, toolkit, package):
+        if package.from_id == 517114114:
+            await toolkit.messages.send(package, End.messages.ok)
+
+            quit()
+
+        await toolkit.messages.send(package, End.messages.no_rights)
+
+        
+
         
