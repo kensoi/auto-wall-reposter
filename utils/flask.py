ab36f59ca072e86a5c8c5a089e2702c5360c252c
Андрей Прокофьев <65781959+kensoi@users.noreply.github.com>
2023-07-23 18:44:57 +0500

    flask app development

---
+++ utils/flask.py
@@ -0,0 +1,63 @@
+"""
+Repl.It forever online patch
+"""
+
+import logging
+import sys
+import os
+
+from flask import Flask
+from threading import Thread
+
+cli = sys.modules['flask.cli']
+cli.show_server_banner = lambda *x: None
+
+app = Flask('Bot online page')
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.CRITICAL)
+
+def set_home_page():
+    global page
+
+    link_to_bot = "https://vk.com/{screen_name}".format(
+        screen_name = os.environ.get("BOT_SCREEN_NAME")
+    )
+
+    form = """
+        <form action="{link_to_bot}">
+            <button type="submit">
+                Open
+            </button>
+        </form>
+    """.format(
+        link_to_bot = link_to_bot
+    )
+
+    page = """
+        <center>
+            <h1>
+                Bot is polling at @{screen_name}
+            </h1>
+            {form}
+        </center>
+    """.format(
+        screen_name = os.environ.get("BOT_SCREEN_NAME"),
+        form = form,
+    )
+
+@app.route('/')
+def home():
+    return page
+
+def keep_alive(debug):
+    if debug:
+        return
+    
+    set_home_page()
+
+    def run():
+        app.run(host='0.0.0.0', port=8080, use_evalex=(not debug))
+        
+    t = Thread(target=run)
+    t.start()
--- utils/replit_patch.py
+++
@@ -1,6 +0,0 @@
-from .background import keep_alive
-
-
-def replit_patch(debug_mode):
-    if not debug_mode:
-        keep_alive(debug_mode)
